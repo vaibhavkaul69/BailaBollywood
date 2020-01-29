d2365830a9195962a225379fe571d0a2fcf43f87
unknown <vaibhav.kaul2@gnmail.com>
2020-01-29 16:26:36 +0530

    Mystery number added in register menu

---
+++ scripts/validate-register-form-and-download-data.js
@@ -0,0 +1,49 @@
+let saveFile = () => 
+        {
+         
+          
+            // Get the data from each element on the form.
+            var name = document.getElementById('txtname');
+            var mail = document.getElementById('txtmail');
+           var number = document.getElementById('txtnum');
+            var password = document.getElementById('txtpass');
+            var mysteryNumber=document.getElementById('txtMystery');
+
+          var nameSize=name.value.length;
+          var mailSize=mail.value.length;
+          var numberSize=number.value.length;
+          var passwordSize=password.value.length;
+            
+            if(nameSize>0 && mailSize>0 && numberSize>0 && passwordSize>0)
+            {
+            // This variable stores all the data.
+            let data = 
+                '\r Name: ' + name.value + ' \r\n ' + 
+                'Email: ' + mail.value + ' \r\n ' + 
+                'Number: ' + number.value + ' \r\n ' + 
+                'Password: ' + password.value + '\r\n '+
+                'Mystery Number: ' + mysteryNumber.value;
+            
+            // Convert the text to BLOB.
+            const textToBLOB = new Blob([data], { type: 'text/plain' });
+            const sFileName = 'BailaUserLoginInfo.txt';	   // The file to save the data.
+    
+            let newLink = document.createElement("a");
+            newLink.download = sFileName;
+    
+            if (window.webkitURL != null) {
+                newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+            }
+            else {
+                newLink.href = window.URL.createObjectURL(textToBLOB);
+                newLink.style.display = "none";
+                document.body.appendChild(newLink);
+            }
+    
+            newLink.click(); 
+          }
+          else
+          {
+            alert("PLEASE ENTER ALL THE DETAILS");
+          }
+        }
