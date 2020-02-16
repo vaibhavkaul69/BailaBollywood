78e01a4ea9fdcb09edf3ee121aff08ce50a22357
unknown <vaibhav.kaul2@gnmail.com>
2020-02-16 14:28:32 +0530

    Dashboard added

---
+++ scripts/redirect-to-forgot-pass.js
@@ -0,0 +1,13 @@
+var forgpass=document.getElementById('emailHelp');
+      var ide_one=document.getElementById('forgotPass');
+      var ide_two=document.getElementById('loginMenu');
+
+      ide_one.style.cssText='display:none;';
+    
+      function recoverPassword()
+    {
+
+        ide_two.style.display='none';
+        ide_one.style.display='block';
+
+    }
